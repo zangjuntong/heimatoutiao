3618717b5fb52710b4b1ac3ef59d1a0c4e56ccca
臧俊通 <1406913232@qq.com>
2019-12-18 22:16:00 +0800

    登录静态页面完成

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,19 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
-
+import Home from '../views/home'
+import Login from '../views/login'
 Vue.use(VueRouter)
 
-const routes = [{
-  path: '/',
-  name: 'home',
-  component: Home
-}
+const routes = [
+  { path: '/', redirect: '/home' },
+  {
+    path: '/home',
+    name: 'home',
+    component: Home
+  }, {
+    path: '/login',
+    component: Login
+  }
 // {
 //   path: '/about',
 //   name: 'about',
---
+++ src/views/login/index.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="main">
+    <el-card class="login-card">
+      <div class="login-img">
+        <img src="../../assets/img/logo_index.png" alt="">
+      </div>
+      <el-form>
+        <el-form-item>
+        <el-input placeholder='请输入手机号'></el-input>
+      </el-form-item>
+       <el-form-item>
+        <el-input style="width:65%" placeholder='请输入验证码'></el-input>
+      <el-button plain style="float:right">发送验证码</el-button>
+      </el-form-item>
+      <el-button style="width:100%" type="primary">主要按钮</el-button>
+      </el-form>
+
+    </el-card>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang='less' scoped>
+ .main {
+   background-image: url('../../assets/img/222.jpg');
+   height: 100vh;
+    background-size: cover;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    .login-card {
+      width: 450px;
+      height: 300px;
+      opacity: 0.5;
+      .login-img {
+        text-align: center;
+        margin-bottom: 30px;
+        img {
+          width: 180px;
+        }
+      }
+    }
+ }
+</style>
