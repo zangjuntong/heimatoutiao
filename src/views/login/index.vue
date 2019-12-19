6fc20e182a2623237593208633c8bed48736da75
臧俊通 <1406913232@qq.com>
2019-12-19 10:49:38 +0800

    该发请求验证用户了

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -4,15 +4,21 @@
       <div class="login-img">
         <img src="../../assets/img/logo_index.png" alt="">
       </div>
-      <el-form>
-        <el-form-item>
-        <el-input placeholder='请输入手机号'></el-input>
+      <el-form ref="myform" :model="loginForm" :rules="loginrules">
+        <el-form-item prop="mobile">
+        <el-input v-model="loginForm.mobile" placeholder='请输入手机号'></el-input>
       </el-form-item>
-       <el-form-item>
-        <el-input style="width:65%" placeholder='请输入验证码'></el-input>
+       <el-form-item prop="passward">
+        <el-input v-model="loginForm.passward" style="width:65%" placeholder='请输入验证码'></el-input>
       <el-button plain style="float:right">发送验证码</el-button>
       </el-form-item>
-      <el-button style="width:100%" type="primary">主要按钮</el-button>
+      <el-form-item prop="checkbox">
+         <el-checkbox v-model="loginForm.checkbox">我已阅读并同意用户协议和隐私条款</el-checkbox>
+      </el-form-item>
+      <el-form-item>
+        <el-button @click="submitLogin" style="width:100%" type="primary">登录</el-button>
+      </el-form-item>
+
       </el-form>
 
     </el-card>
@@ -20,8 +26,39 @@
 </template>
 
 <script>
+// import { log } from 'util'
 export default {
-
+  data () {
+    return {
+      loginForm: {
+        mobile: '',
+        passward: '',
+        checkbox: false
+      },
+      loginrules: {
+        mobile: [{ required: true, message: '请输入手机号' },
+          { pattern: /^1[3456789]\d{9}$/, message: '手机号格式不正确' }],
+        passward: [{ required: true, message: '请输入验证码' },
+          { pattern: /^\d{6}$/, message: '验证码不正确' }],
+        checkbox: [{ validator: function (rule, value, callback) {
+          if (value) {
+            callback()
+          } else {
+            callback(new Error('请选择已同意'))
+          }
+        } }]
+      }
+    }
+  },
+  methods: {
+    submitLogin () {
+      this.$refs.myform.validate(function (isok) {
+        if (isok) {
+          console.log('通过')
+        }
+      })
+    }
+  }
 }
 </script>
 
@@ -35,8 +72,8 @@ export default {
     align-items: center;
     .login-card {
       width: 450px;
-      height: 300px;
-      opacity: 0.5;
+      height: 350px;
+      opacity: 0.7;
       .login-img {
         text-align: center;
         margin-bottom: 30px;
